b1f5a985f622cce23b61941e078533d1e962f0c6
garikoitz <garikoitz@gmail.com>
2018-10-07 22:48:21 -0700

    added doreslice boolean

--- run_docker.sh
+++ run_docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
+# --entrypoint=/bin/bash \
 docker run --rm -it \
-		--entrypoint=/bin/bash \
 		-v /black/localhome/glerma/soft/mrtrix3preproc/config.json:/flywheel/v0/config.json \
         -v /black/localhome/glerma/TESTDATA/mrtrix3preproc/input:/flywheel/v0/input \
         -v /black/localhome/glerma/TESTDATA/mrtrix3preproc/output:/flywheel/v0/output \
-        scitran-apps/mrtrix3preproc
+        scitran-apps/mrtrix3preproc:1.0.1
